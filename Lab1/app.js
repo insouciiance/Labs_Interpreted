ee4263c044ce0f2de4fbf0756dc46d893201e0b2
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-10-20 21:16:43 +0300

    added comments and removed convertToDegrees() method

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,9 +1,5 @@
-const angleInRadians = 3;
+const angleInRadians = 3;                               //угол в радианах
 
-const angleInDegrees = convertToDegrees(angleInRadians);
+const angleInDegrees = angleInRadians * 180 / Math.PI;; //перевод угла из радиан в градусы
 
-console.log(angleInDegrees);
-
-function convertToDegrees(radians) {
-	return radians * 180 / Math.PI;
-}
+console.log(angleInDegrees);                            //вывод числа в градусах
