901f8a3dba032158ca5b111346835ba236289783
insouciiance <49819060+insouciiance@users.noreply.github.com>
2020-10-20 21:25:13 +0300

    encoding

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,5 +1,6 @@
-const angleInRadians = 3;                               //óãîë â ðàäèàíàõ 
+const angleInRadians = 3;                               //угол в радианах
 
-const angleInDegrees = angleInRadians * 180 / Math.PI;; //ïåðåâîä óãëà èç ðàäèàí â ãðàäóñû
+const angleInDegrees = angleInRadians * 180 / Math.PI;; //перевод угла из радиан в градусы
 
-console.log(angleInDegrees);                            //âûâîä ÷èñëà â ãðàäóñàõ
+console.log(angleInDegrees);                            //вывод числа в градусах
+
