fcf5bd62236d90a51a6c3c87d68e4135adca901b
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-10-21 13:30:55 +0300

    added task comment

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,5 +1,7 @@
+//14. Перевести данное значение угла β из радианной меры в градусную
+
 const angleInRadians = 3;                               //угол в радианах
 
 const angleInDegrees = angleInRadians * 180 / Math.PI;; //перевод угла из радиан в градусы
 
-console.log(`Angle in degrees: ${angleInDegrees}`);                            //вывод числа в градусах
+console.log(`Angle in degrees: ${angleInDegrees}`);     //вывод числа в градусах
