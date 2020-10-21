0c61425448d3d8d88d50aff5d7a2b073e0266277
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-10-21 14:28:02 +0300

    added angle in radians output

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,6 +1,7 @@
 //14. Перевести данное значение угла β из радианной меры в градусную
 
 const angleInRadians = 3;                               //угол в радианах
+console.log(`Angle in radians: ${angleInRadians}`);
 
 const angleInDegrees = angleInRadians * 180 / Math.PI;; //перевод угла из радиан в градусы
 
