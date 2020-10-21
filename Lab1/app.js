b2ac90181d6f37832e0430e05231f27e01fe7867
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-10-21 14:44:12 +0300

    removed redundant semi colon

--- Lab1/app.js
+++ Lab1/app.js
@@ -3,6 +3,6 @@
 const angleInRadians = 3;                               //угол в радианах
 console.log(`Angle in radians: ${angleInRadians}`);
 
-const angleInDegrees = angleInRadians * 180 / Math.PI;; //перевод угла из радиан в градусы
+const angleInDegrees = angleInRadians * 180 / Math.PI; //перевод угла из радиан в градусы
 
 console.log(`Angle in degrees: ${angleInDegrees}`);     //вывод числа в градусах
