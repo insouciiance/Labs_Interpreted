ff49aa38ec550a7f72a9f5ba7b8f87a02fc0491c
Nikita Kovalenko <nikkv.ottf@gmail.com>
2020-11-11 01:36:12 +0200

    added lab3 w/o comments

---
+++ Lab3/app.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')();
+
+const a = parseInt(prompt("Enter your number: "));
+
+let x;
+let prevX;
+
+if (a <= 1) {
+    x = Math.min(2 * a, 0.95);
+}
+else if (a > 1 && a < 25) {
+    x = a / 5;
+}
+else {
+    x = a / 25;
+}
+
+do {
+    console.log(x);
+    prevX = x;
+    x = prevX * 4 / 5 + a / (5 * Math.pow(prevX, 4));
+} while (Math.abs(x - prevX) > 0.0001);
+
+console.log(x);
+
+console.log(`The fifth root of ${a} is ${x}`);
